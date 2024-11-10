23db611f81bf5886018f2782f8ae9d277e161ddd
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-11-10 13:55:08 +0100

    some refactoring

--- src/http_handler/http_response/available_slots.rs
+++ src/http_handler/http_response/available_slots.rs
@@ -1,16 +1,8 @@
 use crate::http_handler::serde;
-use crate::http_handler::chrono;
+use crate::http_handler::http_handler_common::CommunicationSlot;
 
 #[derive(serde::Deserialize)]
 pub struct AvailableSlotsResponse{
     communication_slots_used: usize,
     slots: Vec<CommunicationSlot>,
-}
-
-#[derive(serde::Deserialize)]
-struct CommunicationSlot{
-    id: usize,
-    start: chrono::DateTime<chrono::Utc>,
-    end: chrono::DateTime<chrono::Utc>,
-    enabled: bool
 }
--- src/http_handler/mod.rs
+++ src/http_handler/mod.rs
@@ -5,3 +5,4 @@ pub use chrono;
 pub mod http_client;
 pub mod http_response;
 pub mod http_request;
+mod http_handler_common;
