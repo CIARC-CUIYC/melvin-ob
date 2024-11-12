b0f62d4ebdd12fd708e1da492ccb6355f01a3bd2
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-11-12 20:49:53 +0100

    refactoring for http_handler module

--- src/http_handler/mod.rs
+++ src/http_handler/mod.rs
@@ -2,3 +2,8 @@ pub mod http_client;
 pub mod http_response;
 pub mod http_request;
 mod http_handler_common;
+
+pub use http_handler_common::Achievement;
+pub use http_handler_common::ZonedObjective;
+pub use http_handler_common::BeaconObjective;
+pub use http_handler_common::CommunicationSlot;
