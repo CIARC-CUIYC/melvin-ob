a8b495ce43f6fc0d52b225afe5ef0590c06efd3b
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-11-12 10:19:56 +0100

    implements some helper methods for important response subtypes

--- src/http_handler/http_handler_common.rs
+++ src/http_handler/http_handler_common.rs
@@ -1,5 +1,5 @@
-#[derive(serde::Deserialize, serde::Serialize)]
-pub struct ZonedObjective{
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct ZonedObjective {
     id: usize,
     name: String,
     start: chrono::DateTime<chrono::Utc>,
@@ -12,31 +12,66 @@ pub struct ZonedObjective{
     coverage_required: usize,
     description: String,
     sprite: String,
-    secret: bool
+    secret: bool,
 }
 
-#[derive(serde::Deserialize, serde::Serialize)]
-pub struct BeaconObjective{
+impl Timed for ZonedObjective {
+    fn start(&self) -> chrono::DateTime<chrono::Utc> { self.start }
+    fn end(&self) -> chrono::DateTime<chrono::Utc> { self.end }
+}
+
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
+pub struct BeaconObjective {
     id: usize,
     name: String,
     start: chrono::DateTime<chrono::Utc>,
     end: chrono::DateTime<chrono::Utc>,
 }
 
-#[derive(serde::Deserialize)]
-pub struct CommunicationSlot{
+
+impl Timed for BeaconObjective {
+    fn start(&self) -> chrono::DateTime<chrono::Utc> { self.start }
+    fn end(&self) -> chrono::DateTime<chrono::Utc> { self.end }
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct CommunicationSlot {
     id: usize,
     start: chrono::DateTime<chrono::Utc>,
     end: chrono::DateTime<chrono::Utc>,
-    enabled: bool
+    enabled: bool,
 }
 
-#[derive(serde::Deserialize)]
-pub struct Achievement{
+impl CommunicationSlot {
+    fn is_enabled(&self) -> bool { self.enabled }
+}
+
+impl Timed for CommunicationSlot {
+    fn start(&self) -> chrono::DateTime<chrono::Utc> { self.start }
+    fn end(&self) -> chrono::DateTime<chrono::Utc> { self.end }
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct Achievement {
     name: String,
-    done : bool,
+    done: bool,
     points: f32,
     description: String,
     goal_parameter_threshold: bool,
-    goal_parameter: bool
+    goal_parameter: bool,
+}
+
+trait Timed {
+    fn start(&self) -> chrono::DateTime<chrono::Utc>;
+    fn end(&self) -> chrono::DateTime<chrono::Utc>;
+
+    fn time_to_start(&self) -> Option<chrono::Duration> {
+        let now = chrono::Utc::now();
+        if now < self.start() { Some(self.start() - now) } else { None }
+    }
+
+    fn time_to_end(&self) -> Option<chrono::Duration> {
+        let now = chrono::Utc::now();
+        if now < self.end() { Some(self.end() - now) } else { None }
+    }
 }
