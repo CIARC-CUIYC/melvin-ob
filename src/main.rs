6a5f2adb71fece4e328732af2d347049a7699d9d
Felix-Renzikowski <st166260@stud.uni-stuttgart.de>
2024-11-27 14:54:56 +0100

    fixes http observation response bug

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,22 @@ async fn main() {
     const MAP_SIZE_Y: usize = 10800;
 
     let already_covered_map = flight_control::camera_controller::Bitmap::new(MAP_SIZE_X, MAP_SIZE_Y);
-    let http_handler = http_handler::http_client::HTTPClient::new("http://localhost:33000/");
+    let http_handler = http_handler::http_client::HTTPClient::new("http://localhost:33000");
     let controller = flight_control::flight_computer::FlightComputer::new(&http_handler).await;
 
     let mut possible_orbit_coverage_map = flight_control::camera_controller::Bitmap::new(MAP_SIZE_X, MAP_SIZE_Y);
-    let dt: i64 = 0;
+    let mut dt: i64 = 0;
     let start_pos = controller.pos_in_time_delta(chrono::TimeDelta::seconds(0));
 
     while !possible_orbit_coverage_map.data.all() {
         let next_pos = controller.pos_in_time_delta(chrono::TimeDelta::seconds(dt));
-        if next_pos.in_radius_of(&start_pos, 5.0) { break; }
+        if next_pos.in_radius_of(&start_pos, 5.0) && dt >100 { break; }
         possible_orbit_coverage_map.flip_region(next_pos.x() as usize, next_pos.y() as usize);
+        dt += 1;
+        if(dt%10000 == 0){
+            println!("Position: {}, {}", next_pos.x(), next_pos.y());
+        }
     }
-    
+    println!("Done!")
     
 }
